1525f47df3233e2fa28a297b5658f49a8039a4f6
Hazwan Adhikara <hazwanadh@gmail.com>
2024-03-24 13:41:29 +0700

    Sandbox.sh

--- soal_1/Sandbox.sh
+++ soal_1/Sandbox.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-curl -L -o Sandbox.txt 'https://drive.google.com/uc?export=download&id=1cC6MYBI3wRwDgqlFQE1OQUN83JAreId0'
+curl -L -o Sandbox.csv 'https://drive.google.com/uc?export=download&id=1cC6MYBI3wRwDgqlFQE1OQUN83JAreId0'
 
-echo "Contents of Sandbox.txt:"
-cat Sandbox.txt
+echo "Contents of Sandbox.csv:"
+cat Sandbox.csv
 
-awk -F',' 'BEGIN {OFS="|"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20}' Sandbox.txt > hasil.txt
+awk -F',' 'BEGIN {OFS="|"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20}' Sandbox.csv > data.txt
 
-echo -e "\nContents of hasil.txt:"
-cat hasil.txt
+echo -e "\nContents of data.txt:"
+cat data.txt
 
-awk -F'|' '{print $17,$0}' hasil.txt | sort > sales_urut.txt
-echo -e "\nContents of sales_urut.txt:"
-cat sales_urut.txt
+awk -F'|' '{print $17,$6}' data.txt | sort | tail -n 2 > sales.txt
 
-awk -F'|' '{print $7,$20}' hasil.txt | sort -r > segment_urut.txt
-echo -e "\nContents of segment_urut.txt:"
-cat segment_urut.txt
+echo -e "\nTop sales:"
+cat sales.txt
 
-awk -F'|' '{print $14,$20}' hasil.txt | sort | tail -n 3 > category_urut.txt
-echo -e "\nContents of category_urut.txt (top 3):"
-cat category_urut.txt
+awk -F'|' '{print $20,$7}' data.txt | sort | head -n 1 > segment.txt
 
-grep 'Adriaens' hasil.txt | awk -F'|' '{print $2, $18}' > adriaens.txt
-echo -e "\nContents of adriaens.txt:"
+echo -e "\nSegment with smallest value:"
+cat segment.txt
+
+awk -F'|' '{print $20,$14}' data.txt | sort | tail -n 4 > category.txt
+
+echo -e "\nTop 3 categories:"
+cat category.txt
+
+grep 'Adriaens' data.txt | awk -F'|' '{print $2, $18}' > adriaens.txt
+
+echo -e "\nAdriaens:"
 cat adriaens.txt
